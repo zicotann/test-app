bb3640781667e9a6cfc7b04e2dd371e5e75aa71f
Zico Tan <zico.tan@thetechyhub.com>
2019-06-11 16:45:15 +0800

    added header, sidenav and a little content

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,4 +12,17 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+  openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    // document.getElementById("main").style.marginLeft = "250px";
+  }
+
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    // document.getElementById("main").style.marginLeft = "0";
+  }
+
 }
