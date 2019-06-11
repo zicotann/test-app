9e759ba14b7a93691fe31f4faabeb797cddaceaf
Zico Tan <zico.tan@thetechyhub.com>
2019-06-11 19:09:15 +0800

    added list page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from '../app/components/home/home.component'
+import { ListComponent } from '../app/components/list/list.component'
  
 const routes: Routes = [
 
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'songs',
+    component: ListComponent
   }
 
 ];
